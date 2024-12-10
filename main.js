126ef177e9f29035eb49cc6a9cd55533e002044a
Zorishimo <caseyj77@gmail.com>
2024-12-10 14:03:31 +0000

    really basic functions and setup

---
+++ main.js
@@ -0,0 +1,9 @@
+let cssSkillsMatrix = [{
+    id: 1,
+    category: "Layout",
+    skillArea: "Flexbox",
+    description: "Ability to create responsive layouts using flexbox.",
+    skillLevel: "Advanced",
+    demonstration: "Show Example"
+}]
+
